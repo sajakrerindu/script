4d65d551b306d70b5f1e535500a47a03c42dbbb3
sajakrerindu <30977749+sajakrerindu@users.noreply.github.com>
2017-09-08 16:52:38 +0700

    Update safelinkmulti.js

--- safelinkmulti.js
+++ safelinkmulti.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="health-and-fitness-tips-for-women.html"}
 if(x=="2"){xxx="best-interior-design-ideas-2017.html"}
 if(x=="3"){xxx="how-to-make-money-on-youtube.html"}
 if(x=="4"){xxx="do-this-5-forex-beginner-tips-and-save.html"}
-if(x=="5"){xxx="the-perfect-diet-for-teenagersimple-and.html"}a_to_ve[i].href="http://securedownloadgame.blogspot.com/2016/11/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="the-perfect-diet-for-teenagersimple-and.html"}a_to_ve[i].href="https://securedownloadgame.blogspot.com/2016/11/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
